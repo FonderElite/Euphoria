3b8609ee95e1fdcf04ed14cdc3a60d74c7d7fc90
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-03-21 03:28:28 +0000

    Create fastscan.py

---
+++ fastscan.py
@@ -0,0 +1,53 @@
+import os,sys,platform,time
+from colorama import Fore
+import subprocess as sub
+wi="\033[1;37m" #>>White#
+rd="\033[1;31m" #>Red   #
+gr="\033[1;32m" #>Green #
+yl="\033[1;33m" #>Yellow#
+def  slow_print(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(4. / 100)
+def  slow_print2(s): 
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(7. / 100)
+def banner():
+ slow_print(wi + yl + '''
+ ad8888888888ba
+dP'         `"8b,
+8  ,aaa,       "Y888a     ,aaaa,     ,aaa,  ,aa,
+8  8' `8           "8baaaad""""baaaad""""baad""8b
+8  8   8              """"      """"      ""    8b
+8  8, ,8         ,aaaaaaaaaaaaaaaaaaaaaaaaddddd88P
+8  `"""'       ,d8""
+Yb,         ,ad8" 
+ "Y8888888888P"
+______________________
+Vulnerability Scanner
+----------------------
+''')
+banner()
+
+def rust():
+  try:
+   slow_print2(wi + yl + '[!]' + wi + 'Checking some dependencies...')
+   if os.path.exists('/usr/bin/rustscan'):
+    slow_print(wi + gr + '[+]' + wi + 'No missing dependencies.')
+    ip_save = open('ip.txt','r')
+    read = ip_save.read()
+    os.system('rustscan -a' + read)
+    if os.path.exists('/usr/bin/rustscan') == False and os.path.isfile('rustscan_2.0.1_amd64.deb'):
+     slow_print2(wi + gr + '[+]' + wi + 'Unpacking rustscan_2.0.1_amd64.deb')
+     os.system('sudo dpkg -i rustscan_2.0.1_amd64.deb')
+     ip_saved = open('ip.txt','r')
+     readf = ip_saved.read()
+     os.system('rustscan -a' + readf)
+   else:
+     slow_print2(wi + rd + '[+]' + wi + 'Done.')
+  except:
+   print(wi + rd + '[-]' + wi + 'Error.')
+rust()
