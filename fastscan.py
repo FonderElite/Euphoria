bf7c9f1884f04c45f8d5794b29f7f1fe1e0946b4
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-03-21 12:59:45 +0000

    Update fastscan.py

--- fastscan.py
+++ fastscan.py
@@ -9,7 +9,7 @@ def  slow_print(s):
     for c in s + '\n' :
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(4. / 100)
+        time.sleep(6. / 100)
 def  slow_print2(s): 
     for c in s + '\n' :
         sys.stdout.write(c)
@@ -26,10 +26,11 @@ dP'         `"8b,
 8  `"""'       ,d8""
 Yb,         ,ad8" 
  "Y8888888888P"
-______________________
-Vulnerability Scanner
-----------------------
 ''')
+ slow_print(wi + yl + '______________________')
+ slow_print(wi + 'Vulnerability Scanner')
+ slow_print(wi + yl + '----------------------')
+
 banner()
 
 def rust():
@@ -40,16 +41,17 @@ def rust():
     ip_save = open('ip.txt','r')
     read = ip_save.read()
     #os.system('rustscan -a ' + read + ' -- -A -sV -sC --ulimit 5000')
-    sub.call(['rustscan','-a',read,'--','-A','-sV','-sC','--ulimit 5000'])   
+    os.system('rustscan -a ' + read + ' -- -vv -sV -sC -t 5')
    elif os.path.exists('/usr/bin/rustscan') == False and os.path.isfile('rustscan_2.0.1_amd64.deb'):
     slow_print2(wi + gr + '[+]' + wi + 'Unpacking rustscan_2.0.1_amd64.deb')
     os.system('sudo dpkg -i rustscan_2.0.1_amd64.deb')
     ip_saved = open('ip.txt','r')
     readf = ip_saved.read()
     if os.path.exists('/usr/bin/rustscan') == True:
-     sub.call(['rustscan','-a',read,'--','-A','-sV','-sC','--ulimit 5000'])
-   else:
+     os.system('rustscan -a ' + 'localhost' + ' -- -vv -sV -sC -t 5') 
+    else:
      slow_print2(wi + rd + '[+]' + wi + 'Done.')
   except:
    print(wi + rd + '[-]' + wi + 'Error.')
 rust()
+                
